6279bdff26a5e9822d52414c7e33adec4c49885e
Tushar Kashyap <65089058+tusharkashyap63@users.noreply.github.com>
2020-10-23 15:47:21 +0530

    Updates

---
+++ precache-manifest.b763048ef94410aa3a8083662c5a84d9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e90ca7f18ed30cd3188d05b3509b039",
+    "url": "/gitline/index.html"
+  },
+  {
+    "revision": "4f41371270276690fa0e",
+    "url": "/gitline/static/css/main.cf2c18da.chunk.css"
+  },
+  {
+    "revision": "466f19252da57ed0dab8",
+    "url": "/gitline/static/js/2.badbd4de.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/gitline/static/js/2.badbd4de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f41371270276690fa0e",
+    "url": "/gitline/static/js/main.a3b0e815.chunk.js"
+  },
+  {
+    "revision": "eadb8f8f8130a4b95d4f",
+    "url": "/gitline/static/js/runtime-main.ba32023f.js"
+  },
+  {
+    "revision": "bd8706f60fcae58794ff2b0842ba0399",
+    "url": "/gitline/static/media/Asset 1.bd8706f6.svg"
+  },
+  {
+    "revision": "a20e89d437a8ec8b9b13959e4e74d191",
+    "url": "/gitline/static/media/Asset 100.a20e89d4.svg"
+  },
+  {
+    "revision": "b0334a33b24bee3d748b711dfe316b29",
+    "url": "/gitline/static/media/Asset 150.b0334a33.png"
+  },
+  {
+    "revision": "76ec9258cd1ec5a9bf8597a14449d986",
+    "url": "/gitline/static/media/Asset 17.76ec9258.svg"
+  },
+  {
+    "revision": "b25f340f9e0adfd82aea79bf2b714494",
+    "url": "/gitline/static/media/Asset120.b25f340f.png"
+  },
+  {
+    "revision": "8c6941fade5baac8a4e852060e258fee",
+    "url": "/gitline/static/media/Asset70.8c6941fa.png"
+  },
+  {
+    "revision": "64c18dfd186a2beda77c1c7786311cb2",
+    "url": "/gitline/static/media/animat-search.64c18dfd.gif"
+  },
+  {
+    "revision": "51694392cb7135e7f69328c1a503718a",
+    "url": "/gitline/static/media/sparkle1.51694392.png"
+  },
+  {
+    "revision": "7a96ebbe351f836de5b7cda1b45cad3a",
+    "url": "/gitline/static/media/sparkle3.7a96ebbe.png"
+  },
+  {
+    "revision": "82f024e87ba1ccd151f12fa0eba61860",
+    "url": "/gitline/static/media/sparkle4.82f024e8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/repository-timeline/precache-manifest.241ddb164986bf7f8a14c64526a549f6.js"
+  "/gitline/precache-manifest.b763048ef94410aa3a8083662c5a84d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/repository-timeline/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gitline/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
